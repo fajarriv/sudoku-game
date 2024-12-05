5db2d45059d91b25d0b324e53dd0160457df51d8
fajarriv <rivaldiibnusina@gmail.com>
2024-12-05 08:47:13 +0700

    change fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'geist-mono': ['Geist Mono', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 };
